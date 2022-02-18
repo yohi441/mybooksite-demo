2cfe7d8d0b746dac0a6593e2c2ce53b787557f16
yohi441 <rikou92991@gmail.com>
2022-02-18 10:51:35 +0800

    edit settings

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
